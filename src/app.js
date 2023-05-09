9d398cd3b48aab4bf0790cd4bb252b9d39b8aa6e
Poltavskay <poltavskay9@gmail.com>
2023-05-09 20:25:12 +0300

    Added the HTML file

---
+++ src/app.js
@@ -0,0 +1,22 @@
+function fortmatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+        }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+    minutes = `0${minutes}`;
+    }
+    let days = 
+    ["Suunday",
+     "Monday", 
+     "Tuesday",
+     "Wednesday", 
+     "Thursday", 
+     "Friday", 
+     "Saturday" 
+    ];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
