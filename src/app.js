4f62854ab80bf85b761931ea09561d47a60dccdc
Poltavskay <poltavskay9@gmail.com>
2023-05-10 00:28:25 +0300

    Update the code

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-function fortmatDate(timestamp) {
+function formatDate(timestamp) {
     let date = new Date(timestamp);
     let hours = date.getHours();
     if (hours < 10) {
@@ -19,4 +19,72 @@ function fortmatDate(timestamp) {
     ];
     let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
+    }
+
+
+function displayTemperature(response) {
+    let  temperatureElement  = document.querySelector("#temperature");
+    let  cityElement  = document.querySelector("#city");
+    let  descriptionElement  = document.querySelector("#description");
+    let  humidityElement  = document.querySelector("#humidity");
+    let  windElement  = document.querySelector("#wind");
+    let  dateElement  = document.querySelector("#date");
+    let  iconElement  = document.querySelector("#icon");
+  
+  celsiusTemperature = response.data.main.temp;
+
+   temperatureElement.innerHTML = Math.round(celsiusTemperature);
+   cityElement.innerHTML = response.data.name;
+   descriptionElement.innerHTML = response.data.weather[0].description;
+   humidityElement.innerHTML = response.data.main.humidity;
+   windElement.innerHTML =  Math.round(response.data.wind.speed);
+   dateElement.innerHTML = fortmatDate(response.data.dt * 1000);
+   iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+   iconElement.setAttribute("alt",response.data.weather[0].description);
+  console.log(descriptionElement);
+
+}  
+
+function  search(city) {
+    let apiKey = "e9d05d67840ff687d98500d573c2f838";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
 }
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+    
+}
+
+function  displayFahrenheitTemperature(event) {
+event.preventDefault();
+let temperatureElement =document.querySelector("#temperature");
+
+celsiusLink.classList.remove("active");
+fahrenheitLink.classList.add("active");
+let fahrenheitTemperature = ( celsiusTemperature * 9) / 5 + 32;
+temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+event.preventDefault();
+celsiusLink.classList.add("active");
+fahrenheitLink.classList.remove("active");
+let temperatureElement =document.querySelector("#temperature");
+temperatureElement.innerHTML =  Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+   
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("Kyiv");
