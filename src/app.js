54553663c48fe83edb06d6d261d5d616191b816e
Poltavskay <poltavskay9@gmail.com>
2023-05-15 12:21:31 +0300

    Added HTML file in JS

--- src/app.js
+++ src/app.js
@@ -20,6 +20,42 @@ function formatDate(timestamp) {
     ];
     let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
+}
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = ` <div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+   
+      <div class ="col-2">
+      <div class="weather-forecast-date">
+        ${day}
+        </div>
+      <img src="https://openweathermap.org/img/wn/03d@2x.png"
+      alt=""
+      width="42" />
+      <div class="weather-forecast-temperature">
+        <span class="weather-forecast-temperature-max">
+           18°</span>
+        <span class="weather-forecast-temperature-min">
+          12°</span>
+      </div>
+    </div>
+  `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+forecastElement.innerHTML = forecastHTML;
+
+
+
+
+
+
+
   }
   
   
@@ -89,7 +125,7 @@ function formatDate(timestamp) {
   celsiusLink.addEventListener("click", displayCelsiusTemperature);
   
   search("Kyiv");
- 
+displayForecast();
   
   
   
